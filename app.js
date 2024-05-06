046e2509c968cde8f390baaf7aa9d8e3c2ab71e7
EmranSWE <mdemran.swe@gmail.com>
2024-05-07 00:51:08 +0600

    testing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Server running')
+  res.send('Hello World! from testing aws')
 })
 
 app.get("/name",(req,res)=>{
